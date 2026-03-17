2fca63170652f33710dbb43eed37f579a4e505d8
priyansh-chouhan-007 <priyanshchouhan54@gmail.com>
2026-03-17 10:50:14 +0530

    Datatypes committed

---
+++ 02_datatypes/index.js
@@ -0,0 +1,16 @@
+let name="Priyansh"
+const num=5
+var age
+age=45
+console.log(name);
+
+console.table([name,num,age])
+
+console.log(typeof name);
+console.log(typeof num);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof NaN);
+
+
+
