854a07c3d4a269bd6fbff188152c86e22bd85d31
priyansh-chouhan-007 <priyanshchouhan54@gmail.com>
2026-03-17 11:46:23 +0530

    Operations committed

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,20 +1,20 @@
-let score=50
+// let score=50
 //console.log(typeof score);
 //console.log(typeof score);
 
-score="50"
+// score="50"
 //console.log(typeof score);
 //console.log(typeof score);
 
-let valueInNum=Number(score)
+// let valueInNum=Number(score)
 //console.log(typeof valueInNum);
 
 //"33" => 33
 //"33abc" => NaN
 //true => 1 , false => 0
 
-let isLogIn="1"
-let booleanIsLogIn=Boolean(isLogIn)
+// let isLogIn="1"
+// let booleanIsLogIn=Boolean(isLogIn)
 //console.log(booleanIsLogIn);
 
 
@@ -23,8 +23,41 @@ let booleanIsLogIn=Boolean(isLogIn)
 //""=>false
 //"Priyansh"=>true
 
-let someNumber=33;
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33;
+// let stringNumber=String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+//******************** Operations ********************
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="hello"
+// let str2=" Priyansh"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log(1+"2"+2);
+// console.log(1+1+"2");
+// console.log("2"+5+8);
+
+// console.log(1+true);
+// console.log(6+"");
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
