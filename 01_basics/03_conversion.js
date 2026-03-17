22ae6f984ba654f512a7212095fe9a17b0a1783f
priyansh-chouhan-007 <priyanshchouhan54@gmail.com>
2026-03-17 11:10:02 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score=50
+//console.log(typeof score);
+//console.log(typeof score);
+
+score="50"
+//console.log(typeof score);
+//console.log(typeof score);
+
+let valueInNum=Number(score)
+//console.log(typeof valueInNum);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+
+let isLogIn="1"
+let booleanIsLogIn=Boolean(isLogIn)
+//console.log(booleanIsLogIn);
+
+
+//1=>true
+//0=>false
+//""=>false
+//"Priyansh"=>true
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
