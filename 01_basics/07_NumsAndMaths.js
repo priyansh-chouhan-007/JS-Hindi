f9a037e886683f848698fcfd33f562f168580f44
priyansh-chouhan-007 <priyanshchouhan54@gmail.com>
2026-03-19 01:35:46 +0530

    Math Completed

---
+++ 01_basics/07_NumsAndMaths.js
@@ -0,0 +1,30 @@
+// const score=400;
+// console.log(score);
+
+// const balance =new Number(123.54140)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2))
+
+// console.log(balance.toPrecision(3));
+
+//! ******************* MATHS *********************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(2.4));
+// console.log(Math.floor(2.3));
+
+// console.log(Math.min(1,324,1,45,15,154));
+// console.log(Math.max(1,324,1,45,15,154));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=1
+const max=6
+console.log(Math.floor(Math.random()*(max-min+1))+min)
