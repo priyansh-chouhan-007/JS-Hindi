f8b70c0a6769a9908951cfb41e893b5b0f5a606e
priyansh-chouhan-007 <priyanshchouhan54@gmail.com>
2026-03-20 01:05:42 +0530

    Arrays Part 1 Committed

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+//! Array
+const arr=[0,1,2,3,4]
+const heros=['Shaktiman','Naagraj']
+
+// const newArr=new Array(1,2,3,4)
+// console.log(newArr[0]);
+
+
+//!Arrays Methods
+
+// arr.push(5)
+// arr.push(6)
+// arr.pop()
+
+// arr.unshift(7)
+// arr.shift()
+
+// console.log(arr.includes(7));
+// console.log(arr.indexOf(4));
+// console.log(arr);
+
+
+// const newArr=arr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//! Slice,Splice
+console.log("A ",arr);
+const myn1=arr.slice(1,3)
+console.log(myn1);
+console.log("B ",arr);
+const myn2=arr.splice(1,3)
+console.log("C ",arr);
+console.log(myn2);
